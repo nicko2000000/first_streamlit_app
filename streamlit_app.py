e19a649c172f058f8c3965291517d05db9aca1dd
nicko2000000 <37603389+nicko2000000@users.noreply.github.com>
2022-11-21 22:44:46 +1000

    Update streamlit_app.py
    Spell checking

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,11 +4,11 @@ import requests
 import snowflake.connector
 from urllib.error import URLError
 
-streamlit.title('My Parents New Healthy Diner')
+streamlit.title('My Moms New Healthy Diner')
 
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
-streamlit.text('🥗 kale, Spinach & Rocket Smoothie')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
